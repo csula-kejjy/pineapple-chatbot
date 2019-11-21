824b9c4a3b397fb4d9d582339632edf7799953fa
Enrique Castillo <enrique.castillo.rosales64@gmail.com>
2019-11-21 08:14:10 -0800

    Speech-to-text ready for testing, minor refactoring of files, pre-implementation of direct answer module

--- app/modules/chatbot/mysqlstorage.py
+++ app/modules/chatbot/mysqlstorage.py
@@ -1,8 +1,17 @@
+from difflib import SequenceMatcher
 import mysql.connector as mc
+import pandas as pd
 
 class MySQLStorage:
-
+	"""
+	Create a object to query a database and format the data into a DataFrame.
+	"""
 	def __init__(self, **kwargs):
+		"""
+		Initialzie a object that takes in the database's username, password, database name.
+		:param **kwargs: database name, account username, and account password
+		:type **kwargs: str, str, str
+		"""
 		user = kwargs.get('user', 'root')
 		password = kwargs.get('password')
 		database_name = kwargs.get('database')
@@ -22,24 +31,82 @@ class MySQLStorage:
 
 		self.cursor = self.database.cursor(buffered=True)
 
-		self.base_query = "SELECT url, keywords FROM crawler WHERE term = %s"
+	def query(self, intent, entity):
+		"""
+		Returns a DataFrame based on the intent declared by Amazon's Lex
+		:param intent: an intent derived from the user's input which orginates from Amazon's Lex services
+		:type intent: str
+		:param entity: an entity derived from the user's input which orginates from Amazon's Lex services
+		:type entity: str
+		"""
 
-	def get_urls(self, intent, slot):
-		result = []
+		# add '%' wildcard symbols to entity string
+		entity = '%' + entity + '%'
 
-		self.cursor.execute(self.base_query, (intent,))
-		intent_set = set(self.cursor.fetchall())
+		# check which intent is declared and set the base query for that intent
+		if intent == 'GetEmail':
+			query = ("""
+						SELECT DISTINCT
+							url,
+							keywords 
+						FROM 
+							crawler 
+						WHERE 
+							term = 'email'
+						OR
+							term = 'e-mail'
+						AND
+							term = %s
+						AND 
+							keywords LIKE %s
+					""")
+			self.cursor.execute(query, (entity, entity))
 
-		self.cursor.execute(self.base_query, (slot,))
-		slot_set = set(self.cursor.fetchall())
 
-		result_set = slot_set.intersection(intent_set)
+		elif intent == "GetPhoneNumber":
+			query = ("""
+						SELECT DISTINCT
+							url,
+							keywords 
+						FROM 
+							crawler 
+						WHERE 
+							term = phone
+						AND 
+							keywords LIKE %s
+					""")
+			self.cursor.execute(query, (entity,))
 
-		if not result_set:
-			return slot_set
-		else:
-			return result_set
+		elif intent == "GetOfficeRoom":
 
+			query = ("""
+						SELECT DISTINCT
+							url,
+							keywords 
+						FROM 
+							crawler 
+						WHERE 
+							term = office
+						AND 
+							keywords LIKE %s
+					""")
+			self.cursor.execute(query, (entity,))
+				
+
+		# execute query and save data onto variable
+		data = set(self.cursor.fetchall())
+		
+		# remove wildcard '%' symbols from entity string
+		entity = entity.replace('%', '')
+
+		# generate a DataFrame with three columns url, keywords, and score
+		data_df = self.generate_dataframe_with_scores(data, entity)
+
+		# sort the values from highest to lowest based on the 'score' column
+		data_df = data_df.sort_values(by=['score'], ascending=False)
+
+		print(f'\n{data_df}\n')
+		return data_df
 
 	def get_urls2(self, tokens):	
 		result = []
@@ -58,4 +125,57 @@ class MySQLStorage:
 			return result[0].intersection(*result[1:])
 		else:
 			return result[0]
+
+	def generate_dataframe_with_scores(self, data, entity):
+		"""
+		Return a DataFrame with the columns 'url', 'keywords', and 'score' and populate the score column
+		:param data: a set of tuples ('url', 'keywords')
+		:type data: tuple('url', 'keywords')
+		:param entity: an entity derived from the user's input which orginates from Amazon's Lex services
+		:type entity: str
+		"""
+		data_df = pd.DataFrame(columns=['url', 'keywords', 'score'])
+
+		for (url, keywords) in data:
+			text = ""
+			for word in keywords:
+				text += word
+
+			# parse the dataset into a single clean string 
+			text = text.replace("'", "").replace(",", "")
+			data_df = data_df.append(
+					{
+					 'url': url, 
+					 'keywords': text, 
+					 'score': round(SequenceMatcher(None, url, entity).ratio(), 3)*1000
+					 },
+					ignore_index=True)
+
+		return data_df
+
+	def generate_dataframe_without_scores(self, data):
+		"""
+		Return a DataFrame with the columns 'url', 'keywords', and 'score'. Each score is defaulted to 0.
+		:param data: a set of tuples ('url', 'keywords')
+		:type data: tuple('url', 'keywords')
+		"""
+		data_df = pd.DataFrame(columns=['url', 'keywords', 'score'])
+
+		for (url, keywords) in data:
+			text = ""
+			for word in keywords:
+				text += word
+
+			# parse the dataset into a single clean string 
+			text = text.replace("'", "").replace(",", "")
+			data_df = data_df.append(
+					{
+					 'url': url, 
+					 'keywords': text, 
+					 'score': 0
+					 },
+					ignore_index=True)
+
+		return data_df
+
 
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
-from .modules.chatbot.simple_chatbot import SimpleChatBot
 from django.http import HttpResponse
 from django.shortcuts import render
-import boto3, sys, speech_recognition as sr
+from .modules.chatbot.simplechatbot import SimpleChatbot
+import boto3, sys, speech_recognition as sr, time
 
 sys.path.insert(1, '/system')
 
@@ -9,63 +9,26 @@ from system.credentials import Credentials
 
 cred = Credentials()
 # Create your views here.
+bot = SimpleChatbot(user=cred.user, password=cred.password, database=cred.database_name)
+runtime_client = boto3.client("lex-runtime")
 
 def app(request):
 	return render(request, 'app/base.html', {})
 
-def handle_message(request, transcription=None):
+def handle_message(request):
 
 	if request.method == 'POST':
-
-		bot = SimpleChatBot("Chad", user=cred.user, password=cred.password, database=cred.database_name)
-
-		print(transcription)
-		user_message = request.POST['user_message']
-		
-		input_dictionary = {'input': user_message}
-		runtime_client = boto3.client("lex-runtime")
-
-		lex_response = runtime_client.post_text(
-					botName='CSULA_ITS_CHATBOT',
-					botAlias='CHATBOT_DEV',
-					userId='01',
-					inputText=str(input_dictionary['input'])
-				)
-
-		dialogState = str(lex_response.get('dialogState', 'Could not get dialogState from lex response json'))
-
-		# if the dialog state is 'ReadyForFulfillment'
-	
-		if dialogState == 'ReadyForFulfillment':
-
-			# from lex's response, get the 'intentName' parameter, otherwise throw an error.
-			intent = str(lex_response.get('intentName','Could not get intent.'))
-		
-			# from lex's response, get the 'slots' and 'name' parameters, otherwise throw an error.
-			entity = str(lex_response.get('slots','Could not get slot.').get('Name','Could not get slot data.'))
 			
-			print('\n\n\n--\nYour intent is to: ' + intent)
-			print('Your entity is: ' + entity + '\n--\n')
+		try:
+			start_time = time.time()
 
-			# call bot's get_response() method, passing it the user's message, intent and entity
-			bot_response = str(bot.get_response(input_dictionary["input"], intent, entity))
-
-			print(bot_response)
-
-		# if the dialog state is NOT 'ReadyForFulfillment'
-		else:
-
-			# call bot's get_link() method, passing it the user's message
-			bot_response = str(bot.get_link(input_dictionary["input"]))
-			print(bot_response)
-			
-	#self.wfile.write(bytes(bot_response, "utf8"))
-	
-	try:
-		return HttpResponse(bot_response)
-	except:
-		lex_message = lex_response['message']
-		return HttpResponse(lex_message)
+			bot_response = call_lex(request.POST['user_message'])
+			elapsed_time = time.time() - start_time
+			print(f'\n\nTotal time elapsed: {elapsed_time}')
+			return HttpResponse(bot_response)
+		except Exception as e: 
+			print(e)
+			return HttpResponse('Cannot get response from Lex')
 
 	else:
 		print('Request is not a POST method!')
@@ -86,19 +49,72 @@ def handle_listen(request):
 		with sr.Microphone() as source:
 			print("Say something!")
 
+			r.adjust_for_ambient_noise(source)
 			# listen to the microphone and save the recording
 			audio = r.listen(source)
 
 		# recognize speech using Google Speech Recognition
 		try:
+
 			# for testing purposes, we're just using the default API key
 			# to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
 			# instead of `r.recognize_google(audio)`
-			print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-			print(r.recognize_google(audio))
-			#request.POST['user_message'] = r.recognize_google(audio)
-			handle_message(request, r.recognize_google(audio))
+
+			print(f"Google Speech Recognition thinks you said: {r.recognize_google(audio)}")
+			transcription = r.recognize_google(audio)
+			return HttpResponse(transcription)
+
 		except sr.UnknownValueError:
 			print("Google Speech Recognition could not understand audio")
+			return HttpResponse('Google Speech Recognition could not understand audio')
 		except sr.RequestError as e:
-			print("Could not request results from Google Speech Recognition service; {0}".format(e))
+			print(f"Could not request results from Google Speech Recognition service:\n {e}")
+			return HttpResponse(e)
+
+def call_lex(message):
+
+	#if not message: return
+
+	input_dictionary = {'input': message}
+
+	lex_response = runtime_client.post_text(
+				botName='CSULA_ITS_CHATBOT',
+				botAlias='CHATBOT_DEV',
+				userId='01',
+				inputText=str(input_dictionary['input'])
+			)
+
+	print(f"\n\nLex's Response: {lex_response}\n\n")
+
+	dialogState = str(lex_response.get('dialogState', 'Could not get dialogState from lex response json'))
+
+	print(f"\n\nDialog State is: {dialogState}\n\n")
+	# if the dialog state is 'ReadyForFulfillment'
+	
+	if dialogState == 'Fulfilled':
+
+		# from lex's response, get the 'intentName' parameter, otherwise throw an error.
+		intent = str(lex_response.get('intentName','Could not get intent.'))
+		
+		# from lex's response, get the 'slots' and 'name' parameters, otherwise throw an error.
+		entity = str(lex_response.get('slots','Could not get slot.').get('professor_name','Could not get slot data.'))
+			
+		print('\n\n\n--\nYour intent is to: ' + intent)
+		print('Your entity is: ' + entity + '\n--\n')
+
+		# call bot's get_response() method, passing it the user's message, intent and entity
+		bot_response = str(bot.get_response(input_dictionary["input"], intent, entity))
+
+		print(f"\n\nSimpleChatbot's response: {bot_response}\n\n")
+		
+		return bot_response
+
+	# if the dialog state is NOT 'ReadyForFulfillment'
+	else:
+
+		# call bot's get_link() method, passing it the user's message
+		bot_response = str(bot.get_link(input_dictionary["input"]))
+		print(bot_response)
+
+		return bot_response
+
